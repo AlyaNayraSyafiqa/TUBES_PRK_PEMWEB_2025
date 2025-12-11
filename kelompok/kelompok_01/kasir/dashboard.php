f4ca106931747db914653c287096bd3a33d3064e
AlyaNayraSyafiqa <alyaanayra@gmail.com>
2025-12-11 21:19:47 +0700

    mengedit dashboard kasir

--- kelompok/kelompok_01/kasir/dashboard.php
+++ kelompok/kelompok_01/kasir/dashboard.php
@@ -77,7 +77,7 @@ $kategori_penjualan = $conn->query("
             <div class="h-16 flex items-center justify-center bg-pale-taupe">
                 <div class="text-white text-center">
                     <h1 class="text-xl font-bold">EasyResto</h1>
-                    <p class="text-xs text-white opacity-90">Role Kasir</p>
+                    <p class="text-xs text-white opacity-90">Kasir Panel</p>
                 </div>
             </div>
             
